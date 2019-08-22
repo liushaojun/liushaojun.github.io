e702da7b9d26dcdefcc521abdadc23e602ca1012
shaojun.liu <shaojun.liu@ly.com>
2019-08-22 17:19:49 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ MESSAGE="博客更新:`date "+%Y-%m-%d %H:%M:%S"`"
 INPUT_MESSAGE=$1
 COMMIT_MESSAGE=${INPUT_MESSAGE:-$MESSAGE}
 echo $COMMIT_MESSAGE
-git add -A .
+git add .
 git commit -m $COMMIT_MESSAGE
 git push origin hexo-source
